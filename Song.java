3fe32510c16d58ff79e8761f330c38deddd5bb90
Haydenhaugen <haugenh@wlhs.wlwv.k12.or.us>
2026-05-21 13:48:31 -0700

    Correct typos in Song class implementation
    Fix typos in Song class and constructor.

--- Song.java
+++ Song.java
@@ -1,3 +1,15 @@
 public class Song {
+    String title;
+    Strimg artist;
+    String album
+    public Son(String title,String artist,String album){
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+    }
+    @Override
+    public String toString(){
+        return "Title: " + n/this.title
+    }
     
 }
